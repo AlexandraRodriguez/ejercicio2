625ef8c52b9a7a5894f8bb5cb7776b99fffee1d3
patrick cuellar <patrickcuellar@MacBook-Pro-de-patrick.local>
2016-05-28 12:00:52 -0400

    division feature added

--- Aritmetica/src/aritmetica/Aritmetica.java
+++ Aritmetica/src/aritmetica/Aritmetica.java
@@ -11,11 +11,12 @@ package aritmetica;
  */
 public class Aritmetica {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
     
+    public double division(int divisor,int  dividendo){
+        return dividendo/divisor; 
+    }
 }
